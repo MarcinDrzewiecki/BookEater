774781449c044786ada3469e64973881a8d3e8b5
Marcin Drzewiecki <marcin.drzewiecki@gmail.com>
2015-11-28 00:38:25 +0100

    adding cover - quick fix

--- news/urls.py
+++ news/urls.py
@@ -2,13 +2,17 @@ from django.conf.urls import include, url
 from . import views
 from django.conf import settings
 from django.conf.urls import patterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.post_list),
 ]
 
-if settings.DEBUG:
-    # static files (images, css, javascript, etc.)
-    urlpatterns += patterns('',
-        (r'^covers/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT}))
+# if settings.DEBUG:
+#     # static files (images, css, javascript, etc.)
+#     urlpatterns += patterns('',
+#         (r'^covers/(?P<path>.*)$', 'django.views.static.serve', {
+#         'document_root': settings.MEDIA_ROOT}))
+urlpatterns += patterns('',
+    # ... the rest of your URLconf goes here ...
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
